5e1afd97f0665c4d7b00e0c1576aff06ce64ca8f
Jamie <jamieezarate@gmail.com>
2022-06-15 02:16:27 -0500

    working through creation of items in the market

--- Market.sol
+++ Market.sol
@@ -46,4 +46,45 @@ contract MarketPlace is ReentrancyGuard {
     function getListingPrice() public view returns (uint256) {
         return listingPrice;
     }
-}
+
+    function setListingPrice(uint _price) public returns (uint256) {
+        listingPrice = _price;
+        return listingPrice;
+    }
+
+    // TO DO: create function that manages MarketItem
+    function createMarketItem(
+        address nftContract,
+        uint tokenId,
+        uint256 price) public payable nonReentrant {
+        require(price > 0, "Price must be above zero");
+        require(msg.value == listingPrice, "Price must be equal to listing price");
+        
+    
+        //using struct from line 21
+        _itemIds.increment(); //add 1 to the total of items ever created
+        uint256 itemId = _itemIds.current();
+
+        idMarketItem[itemId] = MarketItem (
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable (address(0)), // no owner yet so set as non address
+            price,
+            false);
+        
+        // transfering ownership of the NFT to the contract itself until someone wants to buy.
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+        // log this transaction
+        emit MarketItemCreated(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false);
+        
+        }
